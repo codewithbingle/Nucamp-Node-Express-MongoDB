645a81ac1d6a54621641bf18f8b82da8ca3baa7b
codewithbingle <codewithbingle@gmail.com>
2022-04-16 09:15:22 -0700

    Basic Authentication

---
+++ node-express/node-express/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+
+const hostname = "localhost";
+const port = 3000;
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
+
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ node-mongo/index.js
@@ -0,0 +1,57 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert").strict;
+const dboper = require("./operations");
+
+const url = "mongodb://localhost:27017/";
+const dbname = "nucampsite";
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  assert.strictEqual(err, null);
+
+  console.log("Connected correctly to server");
+
+  const db = client.db(dbname);
+
+  db.dropCollection("campsites", (err, result) => {
+    assert.strictEqual(err, null);
+    console.log("Dropped Collection:", result);
+
+    dboper.insertDocument(
+      db,
+      { name: "Breadcrumb Trail Campground", description: "Test" },
+      "campsites",
+      (result) => {
+        console.log("Insert Document:", result.ops);
+
+        dboper.findDocuments(db, "campsites", (docs) => {
+          console.log("Found Documents:", docs);
+
+          dboper.updateDocument(
+            db,
+            { name: "Breadcrumb Trail Campground" },
+            { description: "Updated Test Description" },
+            "campsites",
+            (result) => {
+              console.log("Updated Document Count:", result.result.nModified);
+
+              dboper.findDocuments(db, "campsites", (docs) => {
+                console.log("Found Documents:", docs);
+
+                dboper.removeDocument(
+                  db,
+                  { name: "Breadcrumb Trail Campground" },
+                  "campsites",
+                  (result) => {
+                    console.log("Deleted Document Count:", result.deletedCount);
+
+                    client.close();
+                  }
+                );
+              });
+            }
+          );
+        });
+      }
+    );
+  });
+});
